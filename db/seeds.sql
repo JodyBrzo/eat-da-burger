6662d9de646a6db2a74bc0e95419b1fc00b55282
Jody Brzovski <jody.brzovski@gmail.com>
2021-03-09 08:43:09 -0500

    npm install, create database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers  (burger_name, devoured) VALUES ('Breakfast Burger', false);
+INSERT INTO burgers  (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers  (burger_name, devoured) VALUES ('Jalapeno Burger', false);
+INSERT INTO burgers  (burger_name, devoured) VALUES ('Mushroom Burger', flase);
+INSERT INTO burgers  (burger_name, devoured) VALUES ('Cowboy Burger', false);
+INSERT INTO burgers  (burger_name, devoured) VALUES ('All American Burger', true);
