11e52484e8b9d2a901a71d863d388c1ae3f06d0d
Jody Brzovski <jody.brzovski@gmail.com>
2021-03-09 14:23:47 -0500

    working on handlebars for get put and post

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,43 @@
 const express = require('express');
-const cat = require('../models/burger.js');
+const burger = require('../models/burger.js');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+      const hbsObject = {
+        burgers: data,
+      };
+      console.log(hbsObject);
+      res.render('index', hbsObject);
+    });
+  });
+  
+  router.post('/api/burgers', (req, res) => {
+    burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+      // Send back the ID of the new burger
+      res.json({ id: result.insertId });
+    });
+  });
+
+  router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    console.log('condition', condition);
+  
+    burger.update(
+      {
+        devoured: req.body.devoured,
+      },
+      condition,
+      (result) => {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      }
+    );
+  });
+  
 
 module.exports = router;
