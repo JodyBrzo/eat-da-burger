91d984a334131fb74711956f0c689851aa94a586
Jody Brzovski <jody.brzovski@gmail.com>
2021-03-09 11:31:20 -0500

    Update burger.js for updateOne ORM call

--- config/models/burger.js
+++ config/models/burger.js
@@ -4,11 +4,12 @@ const butger = {
     selectAll(cb) {
         orm.selectAll('burgers', cols, vals, (res) => cb(res));
     },
-    create(cols, vals cb) {
-        orm.insertOne('burgers' cols, vals (res) => cb(res));
+    create(cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, (res) => cb(res));
     },
-    
+    update(cols, condition, cb) {
+        orm.updateOne('burgers', cols, condition, (res) => cb(res));
+    },
 };
-
 
 module.exports = burger;
