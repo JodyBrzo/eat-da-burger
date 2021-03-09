f62ae3822dd99016acab83aaa27f3a0c78a93c10
Jody Brzovski <jody.brzovski@gmail.com>
2021-03-09 10:59:40 -0500

    orm functions for selectAll and insertOne

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,50 @@
+const connection = require('./connection.js');
+
+
+
 //selectAll()
-
-
+const orm = {
+    selectAll(table, cb) {
+        const queryString = `SELECT * FROM ${table};`;
+        connection.query(queryString, (err, res) => {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
 //insertOne()
+    insertOne(table, cols, vals, cb) {
+        let queryString = `INSERT INTO ${table}`;
 
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        queryString += printQuestionMarks(vals.length);
+        queryString += ') ';
+
+        connection.query(queryString, vals, (err, res) => {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
 
 //updateOne()
+
+
+};
+
+const printQuestionMarks = (num) => {
+    const arr = [];
+  
+    for (let i = 0; i < num; i++) {
+      arr.push('?');
+    }
+  
+    return arr.toString();
+  };
 
 module.exports = orm;
